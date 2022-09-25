dd52ec58c5fb21d9d4ab2288be011c4d3c4cc176
Muhammed Nidhal <94860874+MuhammedNidhal@users.noreply.github.com>
2022-09-26 00:18:38 +0300

    last

--- shop/api/address.py
+++ shop/api/address.py
@@ -1,27 +1,24 @@
-from ast import Try
-from cmath import phase
+from typing import List
 from ninja import Router
 from shop.models import Address, City, Town
-from shop.schemas import AddressIn, AddressOut
+from shop.schemas import AddressIn, AddressOut, MessageOut
 
 address_router = Router(tags=["Address Endpoints"])
 
-@address_router.get("user_addresses/", response=AddressOut)
+@address_router.get("user_addresses/{user_id}", response=List[AddressOut])
 def user_addresses(request, user_id):
     address = Address.objects.filter(user_id=user_id).all()
     
     return address
 
-@address_router.post("new_address/")
+@address_router.post("new_address/", response= MessageOut)
 def new_address(request, address_in: AddressIn):
     userAddress = Address.objects.create(
-        user_id=address_in.user_id,
+        user_id=address_in.user.id,
         name=address_in.name,
-        town=Town.objects.get_or_create(name=address_in.town)[0],
+        town=Town.objects.get_or_create(name=address_in.town.name)[0],
         address=address_in.address,
-        x=address_in.x,
-        y=address_in.y,
         phone=address_in.phone
     )
     
-    return userAddress
+    return {"message": "Address Created!"}
